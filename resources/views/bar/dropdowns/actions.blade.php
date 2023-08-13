4d0a0a66b507759829dd5ed261957c06e54db53c
Ramon Rietdijk <info@ramonrietdijk.nl>
2023-08-13 12:50:13 +0200

    Prevent empty dropdown sections for actions

--- resources/views/bar/dropdowns/actions.blade.php
+++ resources/views/bar/dropdowns/actions.blade.php
@@ -7,30 +7,41 @@
             </svg>
         </x-slot>
         <div class="flex flex-col" x-data="{ selected: @entangle('selected') }">
-            <span
-                class="px-3 py-2 font-bold text-xs uppercase border-b border-neutral-200 bg-neutral-50 text-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
-                @lang('Standalone Actions')
-            </span>
-            @foreach($table['actions']->filter(fn($action): bool => $action->isStandalone()) as $action)
-                <button class="px-3 py-1 text-left truncate hover:bg-neutral-100 dark:hover:bg-neutral-700"
-                        wire:click="executeAction('{{ $action->code() }}')"
-                        x-on:click="show = false">
-                    {{ $action->label() }}
-                </button>
-            @endforeach
-            <span
-                class="px-3 py-2 font-bold text-xs uppercase border-y border-neutral-200 bg-neutral-50 text-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
-                @lang('Actions')
-            </span>
-            @foreach($table['actions']->filter(fn($action): bool => ! $action->isStandalone()) as $action)
-                <button
-                    class="px-3 py-1 text-left truncate hover:bg-neutral-100 disabled:hover:bg-white disabled:text-neutral-500 dark:hover:bg-neutral-700 dark:disabled:hover:bg-neutral-800"
-                    x-bind:disabled="selected.length === 0"
-                    wire:click="executeAction('{{ $action->code() }}')"
-                    x-on:click="show = false">
-                    {{ $action->label() }}
-                </button>
-            @endforeach
+            @php($standaloneActions = $table['actions']->filter(fn($action): bool => $action->isStandalone()))
+            @if($standaloneActions->isNotEmpty())
+                <div class="flex flex-col border-b border-neutral-200 last:border-b-0">
+                    <span
+                        class="px-3 py-2 font-bold text-xs uppercase border-b border-neutral-200 bg-neutral-50 text-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
+                        @lang('Standalone Actions')
+                    </span>
+                    @foreach($standaloneActions as $standaloneAction)
+                        <button class="px-3 py-1 text-left truncate hover:bg-neutral-100 dark:hover:bg-neutral-700"
+                                wire:click="executeAction('{{ $standaloneAction->code() }}')"
+                                x-on:click="show = false">
+                            {{ $standaloneAction->label() }}
+                        </button>
+                    @endforeach
+                </div>
+            @endif
+
+            @php($actions = $table['actions']->filter(fn($action): bool => ! $action->isStandalone()))
+            @if($actions->isNotEmpty())
+                <div class="flex flex-col border-b border-neutral-200 last:border-b-0">
+                    <span
+                        class="px-3 py-2 font-bold text-xs uppercase border-b border-neutral-200 bg-neutral-50 text-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
+                        @lang('Actions')
+                    </span>
+                    @foreach($actions as $action)
+                        <button
+                            class="px-3 py-1 text-left truncate hover:bg-neutral-100 disabled:hover:bg-white disabled:text-neutral-500 dark:hover:bg-neutral-700 dark:disabled:hover:bg-neutral-800"
+                            x-bind:disabled="selected.length === 0"
+                            wire:click="executeAction('{{ $action->code() }}')"
+                            x-on:click="show = false">
+                            {{ $action->label() }}
+                        </button>
+                    @endforeach
+                </div>
+            @endif
         </div>
     </x-livewire-table::dropdown>
 @endif
