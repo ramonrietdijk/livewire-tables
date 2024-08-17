fec974dcba67285f0c95dafe2eb5fc232e5fddef
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-17 10:15:11 +0200

    Column search inputs now always take full width (#74)

--- resources/views/columns/search/date.blade.php
+++ resources/views/columns/search/date.blade.php
@@ -1,5 +1,5 @@
 <div class="px-3 py-1">
     <input type="date"
-           class="w-full border border-neutral-200 rounded-md shadow-sm outline-none bg-white text-black focus:border-blue-300 mr-auto px-2 py-1 font-normal transition ease-in-out dark:bg-neutral-900 dark:border-neutral-700 dark:focus:border-blue-600 dark:text-white h-8"
+           class="min-w-full border border-neutral-200 rounded-md shadow-sm outline-none bg-white text-black focus:border-blue-300 mr-auto px-2 py-1 font-normal transition ease-in-out dark:bg-neutral-900 dark:border-neutral-700 dark:focus:border-blue-600 dark:text-white h-8"
            wire:model.live.debounce.500ms="search.{{ $column->code() }}">
 </div>
