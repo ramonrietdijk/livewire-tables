0c364ff70468214a5fa6a420d9b17ce33a5f9071
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-17 12:16:01 +0200

    Reset filters is only visible if there are filters present (#77)

--- resources/views/bar/dropdowns/filters.blade.php
+++ resources/views/bar/dropdowns/filters.blade.php
@@ -8,10 +8,12 @@
         </x-slot>
         <span
             class="flex gap-2 px-3 py-2 font-bold text-xs uppercase border-b border-neutral-200 bg-neutral-50 text-neutral-500 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-300">
-            <span>@lang('Filters')</span>
-            <a href="#" class="ml-auto text-red-500" x-on:click="show = false" wire:click.prevent="clearFilters">
-                @lang('Clear')
-            </a>
+            <span class="mr-auto">@lang('Filters')</span>
+            @if ($this->canClearFilters())
+                <a href="#" class="text-red-500" x-on:click="show = false" wire:click.prevent="clearFilters">
+                    @lang('Clear')
+                </a>
+            @endif
         </span>
         <div class="flex flex-col">
             @foreach($table['filters'] as $filter)
