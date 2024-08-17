eaf2df2a1589154b838f6162253694d86d732d13
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-17 10:34:05 +0200

    Column values that are null will be displayed as a dash

--- resources/views/columns/content/default.blade.php
+++ resources/views/columns/content/default.blade.php
@@ -1,5 +1,7 @@
 <div class="px-3 py-2 truncate text-black dark:text-white">
-    @if($column->isRaw())
+    @if($value === null)
+        <span class="opacity-25">&mdash;</span>
+    @elseif($column->isRaw())
         {!! $value !!}
     @else
         {{ $value }}
