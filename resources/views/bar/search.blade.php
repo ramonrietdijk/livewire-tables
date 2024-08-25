0bd03c19b6981b43a619997f096f601857640cfa
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-25 18:48:11 +0200

    Only show the global search if any column is searchable

---
+++ resources/views/bar/search.blade.php
@@ -0,0 +1,7 @@
+<div class="flex gap-3">
+    <input type="search"
+        placeholder="@lang('Search all columns...')"
+        class="w-full md:w-64 border border-neutral-200 shadow-sm rounded-md outline-none focus:border-blue-300 px-3 py-2 bg-white text-black transition ease-in-out dark:bg-neutral-800 dark:border-neutral-700 dark:focus:border-blue-600 dark:text-white"
+        wire:model.live.debounce.500ms="globalSearch">
+    @includeWhen($this->canClearSearch(), 'livewire-table::bar.buttons.clear-search')
+</div>
