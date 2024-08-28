2e784fd3f91b0a37885a980a19e493a78f4cc498
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-28 18:09:15 +0200

    Changed icon for the actions dropdown

--- resources/views/bar/dropdowns/actions.blade.php
+++ resources/views/bar/dropdowns/actions.blade.php
@@ -1,9 +1,9 @@
 @if($table['actions']->isNotEmpty())
     <x-livewire-table::dropdown label="{{ __('Actions') }}">
         <x-slot name="icon">
-            <!-- Icon "chevron-double-right" (outline) from https://heroicons.com -->
+            <!-- Icon "play" (outline) from https://heroicons.com -->
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
             </svg>
         </x-slot>
         <div class="flex flex-col" x-data="{ selected: @entangle('selected') }">
