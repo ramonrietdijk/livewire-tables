525ce039084e250974b4cd5a9bdf946a01a17ad6
Ramon Rietdijk <info@ramonrietdijk.nl>
2024-08-29 18:39:50 +0200

    Only enable the selection of records if actions are available

--- tests/Fakes/Livewire/ReorderingBlogLivewireTable.php
+++ tests/Fakes/Livewire/ReorderingBlogLivewireTable.php
@@ -2,6 +2,8 @@
 
 namespace RamonRietdijk\LivewireTables\Tests\Fakes\Livewire;
 
+use Illuminate\Support\Enumerable;
+use RamonRietdijk\LivewireTables\Actions\Action;
 use RamonRietdijk\LivewireTables\Columns\Column;
 use RamonRietdijk\LivewireTables\Livewire\LivewireTable;
 use RamonRietdijk\LivewireTables\Tests\Fakes\Models\Blog;
@@ -18,6 +20,19 @@ class ReorderingBlogLivewireTable extends LivewireTable
             Column::make(__('Title'), 'title')
                 ->sortable()
                 ->searchable(),
+        ];
+    }
+
+    protected function actions(): array
+    {
+        return [
+            Action::make(__('Publish'), 'publish', function (Enumerable $models): void {
+                /** @var Blog $model */
+                foreach ($models as $model) {
+                    $model->published = true;
+                    $model->save();
+                }
+            }),
         ];
     }
 }
