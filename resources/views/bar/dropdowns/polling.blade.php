0bb34be51987c5a46895288f2f5b0aab233f2b6f
Ramon Rietdijk <info@ramonrietdijk.nl>
2025-06-14 11:39:16 +0200

    Add refresh button (#115)

--- resources/views/bar/dropdowns/polling.blade.php
+++ resources/views/bar/dropdowns/polling.blade.php
@@ -1,9 +1,9 @@
 @if(count($pollingOptions) > 0)
     <x-livewire-table::dropdown label="{{ __('Polling') }}">
         <x-slot name="icon">
-            <!-- Icon "arrow-path" (outline) from https://heroicons.com -->
+            <!-- Icon "clock" (outline) from https://heroicons.com -->
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         </x-slot>
         <span
