c0636b7366f537af88d89d075ebd7ec00d75bdea
Ramon Rietdijk <info@ramonrietdijk.nl>
2025-10-19 09:50:47 +0200

    Improve select elements with the multiple attribute

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,5 +1,7 @@
 @props(['size' => 'md'])
 
+@php($multiple = $attributes->has('multiple'))
+
 <label class="block relative">
     <select
         {{
@@ -10,16 +12,20 @@
                 'bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 active:bg-gray-200 dark:active:bg-gray-600',
                 'border-gray-100 dark:border-gray-700 hover:border-gray-200 dark:hover:border-gray-600 focus:border-blue-300 dark:focus:border-blue-400',
                 'text-gray-800 dark:text-white',
-                'pl-3 pr-10 py-2' => $size === 'md',
-                'pl-2 pr-6 py-1' => $size === 'sm',
+                'px-3 py-2' => $size === 'md' && $multiple,
+                'pl-3 pr-10 py-2' => $size === 'md' && ! $multiple,
+                'px-2 py-1' => $size === 'sm' && $multiple,
+                'pl-2 pr-6 py-1' => $size === 'sm' && ! $multiple,
             ])
         }}
     >
         {{ $slot }}
     </select>
-    @if($size === 'sm')
-        <x-livewire-table::icon icon="chevron-down" class="pointer-events-none size-3 absolute right-2 top-3 text-gray-800 dark:text-white transition" />
-    @elseif($size === 'md')
-        <x-livewire-table::icon icon="chevron-down" class="pointer-events-none size-4.5 absolute right-3 top-3 text-gray-800 dark:text-white transition" />
+    @if(! $multiple)
+        @if($size === 'sm')
+            <x-livewire-table::icon icon="chevron-down" class="pointer-events-none size-3 absolute right-2 top-3 text-gray-800 dark:text-white transition" />
+        @elseif($size === 'md')
+            <x-livewire-table::icon icon="chevron-down" class="pointer-events-none size-4.5 absolute right-3 top-3 text-gray-800 dark:text-white transition" />
+        @endif
     @endif
 </label>
--- resources/views/filters/select.blade.php
+++ resources/views/filters/select.blade.php
@@ -1,32 +1,18 @@
 <x-livewire-table::form.group :label="$filter->label()">
-    @if($filter->isMultiple())
-        <x-livewire-table::form.select wire:model.live="filters.{{ $filter->code() }}" multiple>
-            @foreach($filter->getOptions() as $key => $value)
-                @if(is_array($value))
-                    <optgroup wire:key="{{ $key }}" label="{{ $key }}">
-                        @foreach($value as $key2 => $value2)
-                            <option wire:key="{{ $key2 }}" value="{{ $key2 }}">{{ $value2 }}</option>
-                        @endforeach
-                    </optgroup>
-                @else
-                    <option wire:key="{{ $key }}" value="{{ $key }}">{{ $value }}</option>
-                @endif
-            @endforeach
-        </x-livewire-table::form.select>
-    @else
-        <x-livewire-table::form.select wire:model.live="filters.{{ $filter->code() }}">
+    <x-livewire-table::form.select wire:model.live="filters.{{ $filter->code() }}" :multiple="$filter->isMultiple()">
+        @if(! $filter->isMultiple())
             <option value="">&mdash;</option>
-            @foreach($filter->getOptions() as $key => $value)
-                @if(is_array($value))
-                    <optgroup wire:key="{{ $key }}" label="{{ $key }}">
-                        @foreach($value as $key2 => $value2)
-                            <option wire:key="{{ $key2 }}" value="{{ $key2 }}">{{ $value2 }}</option>
-                        @endforeach
-                    </optgroup>
-                @else
-                    <option wire:key="{{ $key }}" value="{{ $key }}">{{ $value }}</option>
-                @endif
-            @endforeach
-        </x-livewire-table::form.select>
-    @endif
+        @endif
+        @foreach($filter->getOptions() as $key => $value)
+            @if(is_array($value))
+                <optgroup wire:key="{{ $key }}" label="{{ $key }}">
+                    @foreach($value as $key2 => $value2)
+                        <option wire:key="{{ $key2 }}" value="{{ $key2 }}">{{ $value2 }}</option>
+                    @endforeach
+                </optgroup>
+            @else
+                <option wire:key="{{ $key }}" value="{{ $key }}">{{ $value }}</option>
+            @endif
+        @endforeach
+    </x-livewire-table::form.select>
 </x-livewire-table::form.group>
