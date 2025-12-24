33d05a2bdfef5789f596cb9edb358f881267a0dc
Ramon Rietdijk <info@ramonrietdijk.nl>
2025-12-24 14:12:08 +0100

    Highlight actions button with a selection (#162)

--- resources/views/toolbar/dropdowns/actions.blade.php
+++ resources/views/toolbar/dropdowns/actions.blade.php
@@ -1,8 +1,12 @@
+@php($actions = $this->resolveActions())
+
 <x-livewire-table::dropdown current="actions">
     <x-livewire-table::button
         :title="__('Actions')"
         :aria-label="__('Actions')"
         x-on:click="toggle"
+        x-data="{{ Js::from(['bulk' => $actions->bulk()->isNotEmpty()]) }}"
+        x-bind:class="{ 'text-blue-500!': bulk && $wire.selected.length > 0 }"
     >
         <x-livewire-table::icon class="size-6" icon="play" />
     </x-livewire-table::button>
