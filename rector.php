a9742071e2c7aadc887a75bd9a581421593e002a
Ramon Rietdijk <info@ramonrietdijk.nl>
2026-02-22 08:25:00 +0100

    Add Rector

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
