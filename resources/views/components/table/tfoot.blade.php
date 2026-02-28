eb31ef330951b18e32b6864f88dc489da86985a2
Ramon Rietdijk <info@ramonrietdijk.nl>
2026-02-28 12:40:32 +0100

    Improve overscroll

--- resources/views/components/table/tfoot.blade.php
+++ resources/views/components/table/tfoot.blade.php
@@ -1,3 +1,3 @@
-<tfoot {{ $attributes->class('bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700 sticky bottom-0 shadow z-20 transition') }}>
+<tfoot {{ $attributes->class('bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700 sticky bottom-0 shadow-[0_-1px_1px_0_rgba(0,0,0,0.05)] z-20 transition') }}>
     {{ $slot }}
 </tfoot>
--- resources/views/components/table/thead.blade.php
+++ resources/views/components/table/thead.blade.php
@@ -1,3 +1,3 @@
-<thead {{ $attributes->class('bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 sticky top-0 shadow z-20 transition') }}>
+<thead {{ $attributes->class('bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 sticky top-0 shadow-[0_1px_1px_0_rgba(0,0,0,0.1)] z-20 transition') }}>
     {{ $slot }}
 </thead>
