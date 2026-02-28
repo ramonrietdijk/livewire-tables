9f1d1042f21b300cb45ce1e16ea1feb34ef8c969
Ramon Rietdijk <info@ramonrietdijk.nl>
2026-02-28 12:43:51 +0100

    Improve overscroll (#179)

--- resources/views/livewire/livewire-table.blade.php
+++ resources/views/livewire/livewire-table.blade.php
@@ -3,7 +3,7 @@
     @if($this->deferLoading) wire:init="init" @endif
     @if(strlen($polling = $this->polling()) > 0) wire:poll.{{ $polling }} @endif
 >
-    <div class="bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm flex flex-col border border-gray-300 dark:border-gray-600 transition">
+    <div class="bg-white dark:bg-gray-800 rounded-md shadow-sm flex flex-col border border-gray-300 dark:border-gray-600 transition">
         @include('livewire-table::toolbar.toolbar')
         <div class="flex-1 overflow-y-auto max-h-179 rounded-b-md">
             @include('livewire-table::table.table')
