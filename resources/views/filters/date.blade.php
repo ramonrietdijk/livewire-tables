75535ce7b93267e9c9e9097bdd74fb25a12cf865
Ramon Rietdijk <info@ramonrietdijk.nl>
2026-03-21 10:48:38 +0100

    Add translatable labels

--- resources/views/filters/date.blade.php
+++ resources/views/filters/date.blade.php
@@ -1,8 +1,8 @@
 <x-livewire-table::dropdown.divider>
-    <x-livewire-table::form.group :label="$filter->label().' (from)'">
+    <x-livewire-table::form.group :label="__(':label (from)', ['label' => $filter->label()])">
         <x-livewire-table::form.input type="date" wire:model.live="filters.{{ $filter->code() }}.from" />
     </x-livewire-table::form.group>
-    <x-livewire-table::form.group :label="$filter->label().' (to)'">
+    <x-livewire-table::form.group :label="__(':label (to)', ['label' => $filter->label()])">
         <x-livewire-table::form.input type="date" wire:model.live="filters.{{ $filter->code() }}.to" />
     </x-livewire-table::form.group>
 </x-livewire-table::dropdown.divider>
