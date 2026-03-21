fd11dd2297f7b2c943eaa76acd771a8e5b11111f
Ramon Rietdijk <info@ramonrietdijk.nl>
2026-03-21 10:20:42 +0100

    Add support for Laravel 13

--- src/Columns/Concerns/HasValue.php
+++ src/Columns/Concerns/HasValue.php
@@ -63,7 +63,7 @@ trait HasValue
         }
 
         if (is_array($value)) {
-            return $value === [] ? null : implode(', ', $value);
+            return $value === [] ? null : implode(', ', $value); // @phpstan-ignore-line
         }
 
         return $value;
